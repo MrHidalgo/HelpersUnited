646c9b7a557e58e775a641035c08a910140196a5
MrHidalgo <Luckyhidalgo@gmail.com>
2019-01-14 14:40:54 +0200

    Initial commit

---
+++ src/js/_lib/webFontLoader.js
@@ -0,0 +1,33 @@
+
+
+/**
+ * @name initWebFontLoader
+ *
+ * @description Loading fonts regardless of the source, then adds a standard set of events you may use to control the loading experience... for more details => https://github.com/typekit/fvd
+ */
+const initWebFontLoader = () => {
+
+	/**
+   * @description
+	 */
+	WebFont.load({
+    google: {
+      families: [
+        'Roboto:100,300,400,500,700,900',
+        'Open+Sans:300,400,600,700,800'
+      ]
+    }
+  });
+
+	/**
+   * @description
+	 */
+  // const WebFontConfig = {
+  //   custom: {
+  //     families: [
+  //       'Lato:n1,n3,n4,n5,n6,n7,n9'
+  //     ]
+  //   }
+  // };
+
+};
